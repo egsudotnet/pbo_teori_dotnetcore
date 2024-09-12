93524be986b458a1d09f2bc3c217d476e208480b
egsu <egsudotnet@gmail.com>
2024-09-12 17:33:43 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.WriteLine("Hello, World!");
+ 
+    // class ProgramA {
+    //     static void Main(string[] args) {
+    //         Console.WriteLine("Hello World! AAA");
+    //     }
+        
+    // }
+
+    class ProgramA {
+        static void Main(string[] args) {
+            SayHello();
+        }
+
+        static void SayHello() {
+            Console.WriteLine("Hello from method!");
+        }
+    
+    }
